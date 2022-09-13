54b662606779aaccd15c117aeaddb7895cdcf4bc
nasroulaye <nasrouminallah70@gmail.com>
2022-09-13 12:32:53 +0000

    encore un changement

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
