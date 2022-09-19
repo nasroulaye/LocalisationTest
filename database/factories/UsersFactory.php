02a9ba31867fc50500c32c3dafd8ccfb816bd112
nasroulaye <nasrouminallah70@gmail.com>
2022-09-19 10:42:26 +0000

    Users Factory

--- database/factories/UsersFactory.php
+++ database/factories/UsersFactory.php
@@ -6,7 +6,7 @@ use App\Models\Users;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-class UserFactory extends Factory
+class UsersFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
@@ -23,7 +23,7 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'nom' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
