c72bfccb06629b1998ff35b3bdf15871fa80a467
nasroulaye <nasrouminallah70@gmail.com>
2022-09-19 15:43:24 +0000

    ddcvc

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,6 @@ Route::post('update-cart', [CartController::class, 'updateCart'])->name('cart.up
 Route::post('remove', [CartController::class, 'removeCart'])->name('cart.remove');
 Route::post('clear', [CartController::class, 'clearAllCart'])->name('cart.clear');
 
-Route::get('/restaurants', [DashboardController::class, 'index']);
+Route::get('/restaurants', [DashboardController::class, 'restaurants']);
 
 require __DIR__.'/auth.php';
