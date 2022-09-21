87343ec8d17fd1afd5f699c644a0385f6ea886d3
nasroulaye <nasrouminallah70@gmail.com>
2022-09-21 17:12:49 +0000

    changement 1

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index')->name('index');
+    return view('index');
 });
 
 Route::get('/restaurants', [DashboardController::class, 'restaurants'])
