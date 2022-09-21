a9df1af9f87e1457f2bbc883f23e59ac31b7cfe6
nasroulaye <nasrouminallah70@gmail.com>
2022-09-21 15:55:17 +0000

    changement 1

---
+++ resources/views/layouts/configs/connection.php
@@ -0,0 +1,35 @@
+<?php
+
+$connect = [
+	# Database Host Name
+	'HOSTNAME' => 'localhost',
+
+	# Database Username
+	'USERNAME' => 'uvrgbdcgjf',
+	//UserName: uvrgbdcgjf
+	//Password : m3uUxzszSj
+	//DBName : uvrgbdcgjf
+	//Password : m3uUxzszSj
+	# Database Password
+	'PASSWORD' => 'm3uUxzszSj',
+
+	# Database Name
+	'DATABASE' => 'uvrgbdcgjf'
+];
+
+# Tables' Prefix
+define('prefix', 'pl_');
+
+# No need to change anything bellow this line
+$db = new mysqli($connect['HOSTNAME'], $connect['USERNAME'], $connect['PASSWORD'], $connect['DATABASE']);
+if($db->connect_errno){
+    echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
+		exit;
+}
+
+
+$sql_mode = $db->query("SELECT @@GLOBAL.sql_mode;");
+$rs_mode = $sql_mode->fetch_assoc();
+if(!empty($rs_mode["@@GLOBAL.sql_mode"])) {
+	$db->query("SET GLOBAL sql_mode='';");
+}
