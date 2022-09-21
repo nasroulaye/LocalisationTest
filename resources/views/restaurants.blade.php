975f6f3e5728a404eea93fdf36125f6c90a0716a
nasroulaye <nasrouminallah70@gmail.com>
2022-09-21 18:33:09 +0000

    changement 1

--- resources/views/restaurants.blade.php
+++ resources/views/restaurants.blade.php
@@ -5,30 +5,33 @@
         </h2>
     </x-slot>
 
-    <div class="py-12" id="shops">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <form class="flex items-center justify-center"
-                  v-on:submit.prevent="fetchShops">
-                <x-input class="py-4 px-6 w-1/2"
-                         placeholder="Find a shop near you"
-                         v-model="shopName" />
-                <x-button class="ml-4 py-4">Search</x-button>
-            </form>
+    <div class="container" id="shops">
+        <div class="row pt-restaurants">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <form class="flex items-center justify-center"
+                    v-on:submit.prevent="fetchShops">
+                    <x-input class="py-4 px-6 w-1/2"
+                            placeholder="Find a shop near you"
+                            v-model="shopName" />
+                    <x-button class="ml-4 py-4">Search</x-button>
+                </form>
 
-            <div class="mt-8 shadow-sm sm:rounded-lg">
-                <div v-show="locationErrorMessage" class="text-center">@{{ locationErrorMessage }}</div>
-                <div v-show="loading" class="text-center">Loading...</div>
-                <div v-show="!loading" class="grid grid-cols-3 gap-4" style="display: none;">
-                    <div class="p-6 bg-white border-b border-gray-200"
-                         v-for="shop in shops"
-                         :key="shop.id">
-                        <div class="text-xl">@{{ shop.name }}</div>
-                        <div class="mt-4 text-gray-500"
-                             v-if="shop.distance">@{{ parseInt(shop.distance).toLocaleString() }}m away</div>
+                <div class="mt-8 shadow-sm sm:rounded-lg">
+                    <div v-show="locationErrorMessage" class="text-center">@{{ locationErrorMessage }}</div>
+                    <div v-show="loading" class="text-center">Loading...</div>
+                    <div v-show="!loading" class="grid grid-cols-3 gap-4" style="display: none;">
+                        <div class="p-6 bg-white border-b border-gray-200"
+                            v-for="shop in shops"
+                            :key="shop.id">
+                            <div class="text-xl">@{{ shop.name }}</div>
+                            <div class="mt-4 text-gray-500"
+                                v-if="shop.distance">@{{ parseInt(shop.distance).toLocaleString() }}m away</div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+        
     </div>
 
     @push('script')
